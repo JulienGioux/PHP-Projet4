6d439794f857893f447e188387d55559831a3f4c
jgx <jules.gioux@gmail.com>
2020-07-01 01:14:57 +0200

    nav

--- index.php
+++ index.php
@@ -4,10 +4,11 @@ $loadedXML = simplexml_load_file($srcXml);
 (isset($_GET['id'])) ? '' : header ("location: 0");
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/nav.css">
     <title><?= $loadedXML -> page[intval($_GET['id'])] -> title ?></title>
 </head>
 <body>
@@ -18,7 +19,10 @@ $loadedXML = simplexml_load_file($srcXml);
     <ul>
     <?php
         foreach ($loadedXML -> page as $key => $value) {
-            echo '<li><a href="' . (intval($value['id']) - 1) . '">' . $value -> menu . '</a></li>';
+    ?>
+    <li><a <?=(intval($_GET['id']) == (intval($value['id']) - 1)) ? 'class="active"' : ''?> href="<?=(intval($value['id']) - 1)?>">
+    <?=$value -> menu?></a></li>
+        <?php
         }
     ?>
     </ul>
