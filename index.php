5ebf70683bf477eb8353b90e8fc0a3249c0b7a49
unknown <jules.gioux@gmail.com>
2020-06-30 16:38:35 +0200

    test

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 $srcXml = 'source.xml';
 $loadedXML = simplexml_load_file($srcXml);
-
-
-
-
+?>
+<?php
 if (isset($_GET['id'])) {
 
     echo $loadedXML -> page[intval($_GET['id'])] -> content;
