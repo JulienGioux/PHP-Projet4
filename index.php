7a8a7faf7319046a089bb76b230db7ebc6d7841b
jgx <jules.gioux@gmail.com>
2020-07-02 12:01:59 +0200

    Vesrsion 1 OK !

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php 
-$srcXml = 'source.xml';
-$loadedXML = simplexml_load_file($srcXml);
+if (file_exists('source.xml')){
+    $srcXml = 'source.xml';
+    $loadedXML = simplexml_load_file($srcXml);
+} else {
+    header ("location: 404.html");
+}
 (isset($_GET['id'])) ? '' : header ("location: 0.html");
 ?>
 <!DOCTYPE html>
@@ -9,25 +13,27 @@ $loadedXML = simplexml_load_file($srcXml);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/nav.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/main.css">
     <title><?= $loadedXML -> page[intval($_GET['id'])] -> title ?></title>
 </head>
 <body>
-<head>
-    <h1><?= $loadedXML -> page[intval($_GET['id'])] -> title ?></h1>
-</head>
+<header class="container  bg-gold">
+    <h1 class="txt-center txt-white font-Title"><?= $loadedXML -> page[intval($_GET['id'])] -> title ?></h1>
+</header>
 <nav>
     <ul>
     <?php
         foreach ($loadedXML -> page as $key => $value) {
     ?>
-    <li><a <?=(intval($_GET['id']) == (intval($value['id']) - 1)) ? 'class="active"' : ''?> href="<?=(intval($value['id']) - 1)?>">
+    <li><a <?=(intval($_GET['id']) == (intval($value['id']) - 1)) ? 'class="active"' : ''?> href="<?=(intval($value['id']) - 1)?>.html">
     <?=$value -> menu?></a></li>
         <?php
         }
     ?>
     </ul>
 </nav>
-<main>
+<main class="container">
     <?= $loadedXML -> page[intval($_GET['id'])] -> content; ?>
 </main>
 </body>
